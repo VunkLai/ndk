f487ab70171ca29778b9ab201312e41ff85b8205
vunk.lai <vunk.lai@zyxel.com.tw>
2020-10-22 14:21:35 +0800

    Added tests to verify that Host and Servcie are correct

--- ndk/objects/host.py
+++ ndk/objects/host.py
@@ -1,9 +1,27 @@
 from enum import Enum
 
-from ndk import fields, core
+from ndk import core, fields
 
 
-class InitialState(Enum):
+class ChoiceMixin:
+
+    @classmethod
+    def choices(cls):
+        for name, member in cls.__members__.items():
+            if not name == 'NO':
+                yield member
+
+    @classmethod
+    def all(cls):
+        items = (member.value for member in cls.choices())
+        return list(items)
+
+    @classmethod
+    def empty(cls):
+        return cls.NO
+
+
+class InitialState(ChoiceMixin, Enum):
     """
     Override the initial state for a Host
 
@@ -21,7 +39,7 @@ class InitialState(Enum):
     UNREACHABLE = 'u'
 
 
-class FlapDetection(Enum):
+class FlapDetection(ChoiceMixin, Enum):
     """
     This directive is used to determine what host states the flap detection logic will use for this host.
 
@@ -36,7 +54,7 @@ class FlapDetection(Enum):
     UNREACHABLE = 'u'
 
 
-class Notification(Enum):
+class Notification(ChoiceMixin, Enum):
     """
     This directive is used to determine when notifications for the host should be sent out.
 
@@ -59,7 +77,7 @@ class Notification(Enum):
     NO = 'n'
 
 
-class Stalking(Enum):
+class Stalking(ChoiceMixin, Enum):
     """
     This directive determines which host states "stalking" is enabled for.
 
@@ -76,7 +94,7 @@ class Stalking(Enum):
     UNREACHABLE = 'u'
 
 
-class NgsHost(core.Object):
+class HostConstruct(core.Object):
     """
     L1 Construct: Nagios::Object::Host
     """
@@ -84,25 +102,25 @@ class NgsHost(core.Object):
     class Meta:
         object_type = 'host'
 
-    host_name = fields.StringField(primary_key=True, requried=True)
-    alias = fields.StringField(requried=True)
+    host_name = fields.StringField(primary_key=True, required=True)
+    alias = fields.StringField(required=True)
     display_name = fields.StringField()
     address = fields.Ipv4Field()
-    parents = fields.ForeignKey('Host')
+    parents = fields.ForeignKey(relation='Host')
     importance = fields.IntegerField()
-    hostgroups = fields.ForeignKey('HostGroup')
-    check_command = fields.ForeignKey('Command')
+    hostgroups = fields.ForeignKey(relation='HostGroup')
+    check_command = fields.ForeignKey(relation='Command')
     initial_state = fields.ChoiceField(choices=InitialState)
-    max_check_attempts = fields.IntegerField(requried=True)
+    max_check_attempts = fields.IntegerField(required=True)
     check_interval = fields.IntegerField()
     retry_interval = fields.IntegerField()
     active_checks_enabled = fields.BooleanField()
     passive_checks_enabled = fields.BooleanField()
-    check_period = fields.ForeignKey('TimePeriod', requried=True)
+    check_period = fields.ForeignKey(relation='TimePeriod', required=True)
     obsess_over_host = fields.BooleanField()
     check_freshness = fields.BooleanField()
     freshness_threshold = fields.IntegerField()
-    event_handler = fields.ForeignKey('Command')
+    event_handler = fields.ForeignKey(relation='Command')
     event_handler_enabled = fields.BooleanField()
     low_flap_threshold = fields.IntegerField()
     high_flap_threshold = fields.IntegerField()
@@ -111,11 +129,13 @@ class NgsHost(core.Object):
     process_perf_data = fields.BooleanField()
     retain_status_information = fields.BooleanField()
     retain_nonstatus_information = fields.BooleanField()
-    contacts = fields.ForeignKey('Contact', composite_key=True)
-    contact_groups = fields.ForeignKey('ContactGroup', composite_key=True)
-    notification_interval = fields.IntegerField(requried=True)
+    contacts = fields.ForeignKey(relation='Contact', composite_key=True)
+    contact_groups = fields.ForeignKey(
+        relation='ContactGroup', composite_key=True)
+    notification_interval = fields.IntegerField(required=True)
     first_notification_delay = fields.IntegerField()
-    notification_period = fields.ForeignKey('TimePeriod', requried=True)
+    notification_period = fields.ForeignKey(
+        relation='TimePeriod', required=True)
     notification_options = fields.ChoiceField(choices=Notification)
     notifications_enabled = fields.BooleanField()
     stalking_options = fields.ChoiceField(choices=Stalking)
@@ -126,72 +146,92 @@ class NgsHost(core.Object):
     icon_image_alt = fields.StringField()
     vrml_image = fields.StringField()
     statusmap_image = fields.StringField()
-    # _2d_coords = fields.ChoiceField(choices=HostOptions, requried=False)
-    # _3d_coords = fields.ChoiceField(choices=HostOptions, requried=False)
+    _2d_coords = fields.StringField()
+    _3d_coords = fields.StringField()
+
+    def __init__(
+            self, stack, host_name, alias, max_check_attempts, check_period,
+            notification_interval, notification_period, display_name=None,
+            address=None, parents=None, importance=None, hostgroups=None,
+            check_command=None, initial_state=None, check_interval=None,
+            retry_interval=None, active_checks_enabled=None,
+            passive_checks_enabled=None, obsess_over_host=None,
+            check_freshness=None, freshness_threshold=None, event_handler=None,
+            event_handler_enabled=None, low_flap_threshold=None,
+            high_flap_threshold=None, flap_detection_enabled=None,
+            flap_detection_options=None, process_perf_data=None,
+            retain_status_information=None, retain_nonstatus_information=None,
+            contacts=None, contact_groups=None, first_notification_delay=None,
+            notification_options=None, notifications_enabled=None,
+            stalking_options=None, notes=None, notes_url=None, action_url=None,
+            icon_image=None, icon_image_alt=None, vrml_image=None,
+            statusmap_image=None, _2d_coords=None, _3d_coords=None):
+
+        super().__init__(
+            stack, host_name=host_name, alias=alias,
+            max_check_attempts=max_check_attempts, check_period=check_period,
+            notification_interval=notification_interval,
+            notification_period=notification_period, display_name=display_name,
+            address=address, parents=parents, importance=importance,
+            hostgroups=hostgroups, check_command=check_command,
+            initial_state=initial_state, check_interval=check_interval,
+            retry_interval=retry_interval,
+            active_checks_enabled=active_checks_enabled,
+            passive_checks_enabled=passive_checks_enabled,
+            obsess_over_host=obsess_over_host, check_freshness=check_freshness,
+            freshness_threshold=freshness_threshold,
+            event_handler=event_handler,
+            event_handler_enabled=event_handler_enabled,
+            low_flap_threshold=low_flap_threshold,
+            high_flap_threshold=high_flap_threshold,
+            flap_detection_enabled=flap_detection_enabled,
+            flap_detection_options=flap_detection_options,
+            process_perf_data=process_perf_data,
+            retain_status_information=retain_status_information,
+            retain_nonstatus_information=retain_nonstatus_information,
+            contacts=contacts, contact_groups=contact_groups,
+            first_notification_delay=first_notification_delay,
+            notification_options=notification_options,
+            notifications_enabled=notifications_enabled,
+            stalking_options=stalking_options, notes=notes,
+            notes_url=notes_url, action_url=action_url, icon_image=icon_image,
+            icon_image_alt=icon_image_alt, vrml_image=vrml_image,
+            statusmap_image=statusmap_image, _2d_coords=_2d_coords,
+            _3d_coords=_3d_coords)
 
 
-class Host(NgsHost):
+class Host(HostConstruct):
     """
     L2 Construct: Nagios::Object::Host
     """
 
-    def __init__(self, stack, host_name=None, alias=None, display_name=None,
-                 address=None, parents=None, importance=None,
-                 hostgroups=None, check_command=None, initial_state=None,
-                 max_check_attempts=None, check_interval=None,
-                 retry_interval=None, active_checks_enabled=None,
-                 passive_checks_enabled=None, check_period=None,
-                 obsess_over_host=None, check_freshness=None,
-                 freshness_threshold=None, event_handler=None,
-                 event_handler_enabled=None, low_flap_threshold=None,
-                 high_flap_threshold=None, flap_detection_enabled=None,
-                 flap_detection_options=None, process_perf_data=None,
-                 retain_status_information=None,
-                 retain_nonstatus_information=None,
-                 contacts=None, contact_groups=None,
-                 notification_interval=None, first_notification_delay=None,
-                 notification_period=None, notification_options=None,
-                 notifications_enabled=None, stalking_options=None,
-                 notes=None, notes_url=None, action_url=None,
-                 icon_image=None, icon_image_alt=None, vrml_image=None,
-                 statusmap_image=None,
-                 # _2d_coords=None, # _3d_coords=None
-                 ):
-
-        super().__init__(stack, host_name=host_name, alias=alias,
-                         display_name=display_name, address=address,
-                         parents=parents, importance=importance,
-                         hostgroups=hostgroups, check_command=check_command,
-                         initial_state=initial_state,
-                         max_check_attempts=max_check_attempts,
-                         check_interval=check_interval,
-                         retry_interval=retry_interval,
-                         active_checks_enabled=active_checks_enabled,
-                         passive_checks_enabled=passive_checks_enabled,
-                         check_period=check_period,
-                         obsess_over_host=obsess_over_host,
-                         check_freshness=check_freshness,
-                         freshness_threshold=freshness_threshold,
-                         event_handler=event_handler,
-                         event_handler_enabled=event_handler_enabled,
-                         low_flap_threshold=low_flap_threshold,
-                         high_flap_threshold=high_flap_threshold,
-                         flap_detection_enabled=flap_detection_enabled,
-                         flap_detection_options=flap_detection_options,
-                         process_perf_data=process_perf_data,
-                         retain_status_information=retain_status_information,
-                         retain_nonstatus_information=retain_nonstatus_information,
-                         contacts=contacts, contact_groups=contact_groups,
-                         notification_interval=notification_interval,
-                         first_notification_delay=first_notification_delay,
-                         notification_period=notification_period,
-                         notification_options=notification_options,
-                         notifications_enabled=notifications_enabled,
-                         stalking_options=stalking_options, notes=notes,
-                         notes_url=notes_url, action_url=action_url,
-                         icon_image=icon_image,
-                         icon_image_alt=icon_image_alt,
-                         vrml_image=vrml_image,
-                         statusmap_image=statusmap_image,
-                         # _2d_coords=_2d_coords, _3d_coords=_3d_coords,
-                         )
+    def __init__(
+            self, stack, host_name, address, notification_period,
+            check_command, max_check_attempts=5, check_period=3,
+            notification_interval=60, alias=None, check_interval=None,
+            retry_interval=None, active_checks_enabled=True,
+            passive_checks_enabled=False, event_handler=None,
+            event_handler_enabled=False, flap_detection_enabled=True,
+            flap_detection_options=None, contacts=None, contact_groups=None,
+            first_notification_delay=None, notifications_enabled=True,
+            notification_options=Notification.all(),
+            **kwargs):
+        alias = alias or host_name
+        super().__init__(
+            stack, host_name=host_name, alias=alias, address=address,
+            max_check_attempts=max_check_attempts, check_period=check_period,
+            notification_interval=notification_interval,
+            notification_period=notification_period,
+            check_command=check_command,
+            check_interval=check_interval,
+            retry_interval=retry_interval,
+            active_checks_enabled=active_checks_enabled,
+            passive_checks_enabled=passive_checks_enabled,
+            event_handler=event_handler,
+            event_handler_enabled=event_handler_enabled,
+            flap_detection_enabled=flap_detection_enabled,
+            flap_detection_options=flap_detection_options,
+            contacts=contacts, contact_groups=contact_groups,
+            first_notification_delay=first_notification_delay,
+            notification_options=notification_options,
+            notifications_enabled=notifications_enabled, **kwargs)
--- ndk/objects/service.py
+++ ndk/objects/service.py
@@ -1,9 +1,27 @@
 from enum import Enum
 
-from ndk import fields, core
+from ndk import core, fields
 
 
-class InitialState(Enum):
+class ChoiceMixin:
+
+    @classmethod
+    def choices(cls):
+        for name, member in cls.__members__.items():
+            if not name == 'NO':
+                yield member
+
+    @classmethod
+    def all(cls):
+        items = (member.value for member in cls.choices())
+        return list(items)
+
+    @classmethod
+    def empty(cls):
+        return cls.NO
+
+
+class InitialState(ChoiceMixin, Enum):
     """
     Override the initial state for a Host
 
@@ -23,7 +41,7 @@ class InitialState(Enum):
     CRITICAL = 'c'
 
 
-class FlapDetection(Enum):
+class FlapDetection(ChoiceMixin, Enum):
     """
     This directive is used to determine what service states the flap detection
     logic will use for this service.
@@ -41,7 +59,7 @@ class FlapDetection(Enum):
     CRITICAL = 'c'
 
 
-class Notification(Enum):
+class Notification(ChoiceMixin, Enum):
     """
     This directive is used to determine
     when notifications for the service should be sent out.
@@ -69,7 +87,7 @@ class Notification(Enum):
     NO = 'n'
 
 
-class Stalking(Enum):
+class Stalking(ChoiceMixin, Enum):
     """
     This directive determines which service states "stalking" is enabled for.
 
@@ -88,7 +106,7 @@ class Stalking(Enum):
     CRITICAL = 'c'
 
 
-class NgsService(core.Object):
+class ServiceConstruct(core.Object):
     """
     L1 Construct: Nagios::Object::Service
     """
@@ -96,26 +114,26 @@ class NgsService(core.Object):
     class Meta:
         object_type = 'service'
 
-    host_name = fields.StringField(primary_key=True, requried=True)
-    hostgroup_name = fields.ForeignKey('HostGroup')
-    service_description = fields.StringField(primary_key=True, requried=True)
+    host_name = fields.StringField(primary_key=True, required=True)
+    hostgroup_name = fields.ForeignKey(relation='HostGroup')
+    service_description = fields.StringField(primary_key=True, required=True)
     display_name = fields.StringField()
-    parents = fields.ForeignKey('Service')
+    parents = fields.ForeignKey(relation='Service')
     importance = fields.IntegerField()
-    servicegroups = fields.ForeignKey('ServiceGroup')
+    servicegroups = fields.ForeignKey(relation='ServiceGroup')
     is_volatile = fields.BooleanField()
-    check_command = fields.StringField(requried=True)
+    check_command = fields.StringField(required=True)
     initial_state = fields.ChoiceField(choices=InitialState)
-    max_check_attempts = fields.IntegerField(requried=True)
-    check_interval = fields.IntegerField(requried=True)
-    retry_interval = fields.IntegerField(requried=True)
+    max_check_attempts = fields.IntegerField(required=True)
+    check_interval = fields.IntegerField(required=True)
+    retry_interval = fields.IntegerField(required=True)
     active_checks_enabled = fields.BooleanField()
     passive_checks_enabled = fields.BooleanField()
-    check_period = fields.ForeignKey('TimePeriod', requried=True)
+    check_period = fields.ForeignKey(relation='TimePeriod', required=True)
     obsess_over_service = fields.BooleanField()
     check_freshness = fields.BooleanField()
     freshness_threshold = fields.IntegerField()
-    event_handler = fields.ForeignKey('Command')
+    event_handler = fields.ForeignKey(relation='Command')
     event_handler_enabled = fields.BooleanField()
     low_flap_threshold = fields.IntegerField()
     high_flap_threshold = fields.IntegerField()
@@ -125,14 +143,16 @@ class NgsService(core.Object):
     process_perf_data = fields.BooleanField()
     retain_status_information = fields.BooleanField()
     retain_nonstatus_information = fields.BooleanField()
-    notification_interval = fields.IntegerField(requried=True)
+    notification_interval = fields.IntegerField(required=True)
     first_notification_delay = fields.IntegerField()
-    notification_period = fields.ForeignKey('TimePeriod', requried=True)
+    notification_period = fields.ForeignKey(
+        relation='TimePeriod', required=True)
     notification_options = fields.ChoiceField(
         choices=Notification)
     notifications_enabled = fields.BooleanField()
-    contacts = fields.ForeignKey('Contact', composite_key=True)
-    contact_groups = fields.ForeignKey('ContactGroup', composite_key=True)
+    contacts = fields.ForeignKey(relation='Contact', composite_key=True)
+    contact_groups = fields.ForeignKey(
+        relation='ContactGroup', composite_key=True)
     stalking_options = fields.ChoiceField(choices=Stalking)
     notes = fields.StringField()
     notes_url = fields.StringField()
@@ -140,92 +160,84 @@ class NgsService(core.Object):
     icon_image = fields.StringField()
     icon_image_alt = fields.StringField()
 
+    def __init__(
+            self, stack, host_name, service_description, check_command,
+            check_interval, retry_interval, check_period,
+            notification_interval, notification_period, hostgroup_name=None,
+            display_name=None, parents=None, importance=None,
+            servicegroups=None, is_volatile=None, initial_state=None,
+            max_check_attempts=None, active_checks_enabled=None,
+            passive_checks_enabled=None, obsess_over_service=None,
+            check_freshness=None, freshness_threshold=None, event_handler=None,
+            event_handler_enabled=None, low_flap_threshold=None,
+            high_flap_threshold=None, flap_detection_enabled=None,
+            flap_detection_options=None, process_perf_data=None,
+            retain_status_information=None, retain_nonstatus_information=None,
+            first_notification_delay=None, notification_options=None,
+            notifications_enabled=None, contacts=None, contact_groups=None,
+            stalking_options=None, notes=None, notes_url=None, action_url=None,
+            icon_image=None, icon_image_alt=None):
+        super().__init__(
+            stack, host_name=host_name, hostgroup_name=hostgroup_name,
+            service_description=service_description, display_name=display_name,
+            parents=parents, importance=importance,
+            servicegroups=servicegroups, is_volatile=is_volatile,
+            check_command=check_command, initial_state=initial_state,
+            max_check_attempts=max_check_attempts,
+            check_interval=check_interval, retry_interval=retry_interval,
+            active_checks_enabled=active_checks_enabled,
+            passive_checks_enabled=passive_checks_enabled,
+            check_period=check_period, obsess_over_service=obsess_over_service,
+            check_freshness=check_freshness,
+            freshness_threshold=freshness_threshold,
+            event_handler=event_handler,
+            event_handler_enabled=event_handler_enabled,
+            low_flap_threshold=low_flap_threshold,
+            high_flap_threshold=high_flap_threshold,
+            flap_detection_enabled=flap_detection_enabled,
+            flap_detection_options=flap_detection_options,
+            process_perf_data=process_perf_data,
+            retain_status_information=retain_status_information,
+            retain_nonstatus_information=retain_nonstatus_information,
+            notification_interval=notification_interval,
+            first_notification_delay=first_notification_delay,
+            notification_period=notification_period,
+            notification_options=notification_options,
+            notifications_enabled=notifications_enabled, contacts=contacts,
+            contact_groups=contact_groups, stalking_options=stalking_options,
+            notes=notes, notes_url=notes_url, action_url=action_url,
+            icon_image=icon_image, icon_image_alt=icon_image_alt)
 
-class Service(NgsService):
+
+class Service(ServiceConstruct):
     """
     L2 Construct: Nagios::Object::Service
     """
 
-    def __init__(self, stack, host_name=None,
-                 hostgroup_name=None,
-                 service_description=None,
-                 display_name=None,
-                 parents=None,
-                 importance=None,
-                 servicegroups=None,
-                 is_volatile=None,
-                 check_command=None,
-                 initial_state=None,
-                 max_check_attempts=None,
-                 check_interval=None,
-                 retry_interval=None,
-                 active_checks_enabled=None,
-                 passive_checks_enabled=None,
-                 check_period=None,
-                 obsess_over_service=None,
-                 check_freshness=None,
-                 freshness_threshold=None,
-                 event_handler=None,
-                 event_handler_enabled=None,
-                 low_flap_threshold=None,
-                 high_flap_threshold=None,
-                 flap_detection_enabled=None,
-                 flap_detection_options=None,
-                 process_perf_data=None,
-                 retain_status_information=None,
-                 retain_nonstatus_information=None,
-                 notification_interval=None,
-                 first_notification_delay=None,
-                 notification_period=None,
-                 notification_options=None,
-                 notifications_enabled=None,
-                 contacts=None,
-                 contact_groups=None,
-                 stalking_options=None,
-                 notes=None,
-                 notes_url=None,
-                 action_url=None,
-                 icon_image=None,
-                 icon_image_alt=None):
-        super().__init__(stack,
-                         host_name=host_name,
-                         hostgroup_name=hostgroup_name,
-                         service_description=service_description,
-                         display_name=display_name,
-                         parents=parents,
-                         importance=importance,
-                         servicegroups=servicegroups,
-                         is_volatile=is_volatile,
-                         check_command=check_command,
-                         initial_state=initial_state,
-                         max_check_attempts=max_check_attempts,
-                         check_interval=check_interval,
-                         retry_interval=retry_interval,
-                         active_checks_enabled=active_checks_enabled,
-                         passive_checks_enabled=passive_checks_enabled,
-                         check_period=check_period,
-                         obsess_over_service=obsess_over_service,
-                         check_freshness=check_freshness,
-                         freshness_threshold=freshness_threshold,
-                         event_handler=event_handler,
-                         event_handler_enabled=event_handler_enabled,
-                         low_flap_threshold=low_flap_threshold,
-                         high_flap_threshold=high_flap_threshold,
-                         flap_detection_enabled=flap_detection_enabled,
-                         flap_detection_options=flap_detection_options,
-                         process_perf_data=process_perf_data,
-                         retain_status_information=retain_status_information,
-                         retain_nonstatus_information=retain_nonstatus_information,
-                         notification_interval=notification_interval,
-                         first_notification_delay=first_notification_delay,
-                         notification_period=notification_period,
-                         notification_options=notification_options,
-                         notifications_enabled=notifications_enabled,
-                         contacts=contacts,
-                         contact_groups=contact_groups,
-                         stalking_options=stalking_options,
-                         notes=notes,
-                         notes_url=notes_url,
-                         action_url=action_url,
-                         icon_image=icon_image,
-                         icon_image_alt=icon_image_alt)
+    def __init__(
+            self, stack, host_name, service_description, check_command,
+            notification_period, check_interval=3, retry_interval=2,
+            check_period=5, notification_interval=60, max_check_attempts=5,
+            active_checks_enabled=True, passive_checks_enabled=False,
+            event_handler=None, event_handler_enabled=False,
+            flap_detection_enabled=True, flap_detection_options=None,
+            first_notification_delay=None, notification_options=None,
+            notifications_enabled=True, contacts=None, contact_groups=None, **
+            kwargs):
+        super().__init__(
+            stack, host_name=host_name,
+            service_description=service_description,
+            check_command=check_command, max_check_attempts=max_check_attempts,
+            check_interval=check_interval, retry_interval=retry_interval,
+            active_checks_enabled=active_checks_enabled,
+            passive_checks_enabled=passive_checks_enabled,
+            check_period=check_period, event_handler=event_handler,
+            event_handler_enabled=event_handler_enabled,
+            flap_detection_enabled=flap_detection_enabled,
+            flap_detection_options=flap_detection_options,
+            notification_interval=notification_interval,
+            first_notification_delay=first_notification_delay,
+            notification_period=notification_period,
+            notification_options=notification_options,
+            notifications_enabled=notifications_enabled, contacts=contacts,
+            contact_groups=contact_groups, **kwargs)
