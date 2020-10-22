98ddca3cc0cdf733057356c6f1229f969d435cb2
vunk.lai <vunk.lai@zyxel.com.tw>
2020-10-22 13:40:40 +0800

    Added tests to verify that HostGroup and ServcieGroup are correct

--- ndk/objects/hostgroup.py
+++ ndk/objects/hostgroup.py
@@ -1,7 +1,7 @@
-from ndk import fields, core
+from ndk import core, fields
 
 
-class NgsHostGroup(core.Object):
+class HostGroupConstruct(core.Object):
     """
     L1 Construct: Nagios::Object::HostGroup
     """
@@ -9,19 +9,28 @@ class NgsHostGroup(core.Object):
     class Meta:
         object_type = 'hostgroup'
 
-    hostgroup_name = fields.StringField(primary_key=True, requried=True)
-    alias = fields.StringField(requried=True)
-    members = fields.ForeignKey('Host')
-    hostgroup_members = fields.ForeignKey('HostGroup')
+    hostgroup_name = fields.StringField(primary_key=True, required=True)
+    alias = fields.StringField(required=True)
+    members = fields.ForeignKey(relation='Host')
+    hostgroup_members = fields.ForeignKey(relation='HostGroup')
     notes = fields.StringField()
     notes_url = fields.StringField()
     action_url = fields.StringField()
 
+    def __init__(self, stack, hostgroup_name, alias, members=None,
+                 hostgroup_members=None, notes=None, notes_url=None,
+                 action_url=None):
+        super().__init__(stack, hostgroup_name=hostgroup_name, alias=alias,
+                         members=members, hostgroup_members=hostgroup_members,
+                         notes=notes, notes_url=notes_url, action_url=action_url)
 
-class HostGroup(core.Object):
+
+class HostGroup(HostGroupConstruct):
     """
     L2 Construct: Nagios::Object::HostGroup
     """
 
-    def __init__(self, stack, hostgroup_name, alias, members=None, hostgroup_members=None, notes=None, notes_url=None, action_url=None):
-        super().__init__(stack, hostgroup_name=hostgroup_name, alias=alias, members=members, hostgroup_members=hostgroup_members, notes=notes, notes_url=notes_url, action_url=action_url):
+    def __init__(self, stack, hostgroup_name, alias, **kwargs):
+        alias = alias or hostgroup_name
+        super().__init__(
+            stack, hostgroup_name=hostgroup_name, alias=alias, **kwargs)
