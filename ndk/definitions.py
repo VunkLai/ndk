3b4dea27cc11350d9e258d090d5ba03e6b1a0b4c
vunk.lai <vunk.lai@zyxel.com.tw>
2020-10-27 11:04:18 +0800

    Refactored Fields definitions.py
    Split logic into separate functions to clarify and allow reuse.

--- ndk/definitions.py
+++ ndk/definitions.py
@@ -3,78 +3,10 @@ from enum import Enum
 from importlib import import_module
 
 import attr
-from attr import converters
-from attr import ib as field
-from attr import validators
+from attr import converters, validators
 
 from ndk.construct import Construct
-
-
-class Converters:
-    STR = converters.optional(str)
-    INT = converters.optional(int)
-    BOOL = converters.optional(bool)
-
-    @staticmethod
-    def PrimaryKey(name):
-        return str(name).lower().replace(' ', '-')
-
-
-def PrimaryKey():
-    return field(type=str, converter=Converters.PrimaryKey)
-
-
-def StringField(required=False):
-    if required:
-        return field(type=str, converter=str, kw_only=True)
-    return field(
-        type=str, converter=Converters.STR, default=None, kw_only=True)
-
-
-def IntegerField(required=False):
-    if required:
-        return field(type=int, converter=int, kw_only=True)
-    return field(
-        type=int, converter=Converters.INT, default=None, kw_only=True)
-
-
-def BooleanField(required=False):
-    if required:
-        return field(type=bool, converter=bool, kw_only=True)
-    return field(
-        type=bool, converter=Converters.BOOL, default=None, kw_only=True)
-
-
-def ForeignKey(relation, required=False):
-    cls = relation
-    if isinstance(relation, str):
-        module = import_module('.definitions', package='ndk')
-        cls = getattr(module,  f'{relation}Directive')
-
-    if required:
-        return field(type=cls, converter=cls.converter, kw_only=True)
-    return field(type=cls, converter=converters.optional(cls.converter),
-                 default=None, kw_only=True)
-
-
-def ChoiceField(items, required=False):
-    if required:
-        return field(
-            type=typing.List[items],
-            validator=validators.deep_iterable(
-                member_validator=validators.instance_of(items),
-                iterable_validator=validators.instance_of(list))
-        )
-    else:
-        return field(
-            type=items,
-            validator=validators.optional(
-                validators.deep_iterable(
-                    member_validator=validators.instance_of(items),
-                    iterable_validator=validators.instance_of(list))
-            ),
-            default=None,
-            kw_only=True)
+from ndk.directives import *
 
 
 @attr.s
